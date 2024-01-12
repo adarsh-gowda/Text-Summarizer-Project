ec476d48b7ec20756697dcfd7f2ff82d3eba0dff
Adarsha C V <A12EJ56K@airtel.com>
2024-01-12 14:02:40 +0530

    model evaluation done

--- src/textSummarizer/conponents/model_trainer.py
+++ src/textSummarizer/conponents/model_trainer.py
@@ -4,6 +4,7 @@ from transformers import AutoTokenizer,AutoModelForSeq2SeqLM
 from datasets import load_dataset, load_from_disk
 from textSummarizer.entity import ModelTrainerConfig
 import torch
+import os
 
 
 class ModelTrainer:
--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -34,3 +34,13 @@ class ModelTrainerConfig:
     eval_steps : int
     save_steps : int
     gradient_accumation_steps : int
+
+
+@dataclass(frozen=True)
+class ModelEvaluationConfig:
+    root_dir : Path
+    data_path : Path
+    model_path : Path
+    tokenizer_path : Path
+    metric_file_name : Path
+    
